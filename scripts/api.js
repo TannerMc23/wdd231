8e4c59e84e80f6c30d75ec148cced999db3a010e
tannermc23 <tmcmillan@byupathway.edu>
2026-01-19 22:44:41 -0700

    Completed as far as I could get. Will finish tomorrow.

---
+++ scripts/api.js
@@ -0,0 +1,21 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat={49.75}&lon={6.64}&appid={29e1cce74baaef393d4c3d7c4800cfb5}';
+
+async function apiFetch() {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data); // testing only
+      // displayResults(data); // uncomment when ready
+    } else {
+        throw Error(await response.text());
+    }
+  } catch (error) {
+      console.log(error);
+  }
+}
+
+apiFetch();
