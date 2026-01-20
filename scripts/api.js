aa5ab15de9f9e2bf3c91190826420f56d9aafaf7
tannermc23 <tmcmillan@byupathway.edu>
2026-01-20 11:16:50 -0700

    Finished API activity.

--- scripts/api.js
+++ scripts/api.js
@@ -1,7 +1,10 @@
 const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
-const url = 'https://api.openweathermap.org/data/2.5/weather?lat={49.75}&lon={6.64}&appid={29e1cce74baaef393d4c3d7c4800cfb5}';
+const myKey = "29e1cce74baaef393d4c3d7c4800cfb5";
+const myLat = "49.75";
+const myLong = "6.64";
+const url = `//api.openweathermap.org/data/2.5/weather?lat=${myLat}&lon=${myLong}&appid=${myKey}&units=imperial`;
 
 async function apiFetch() {
   try {
@@ -9,13 +12,21 @@ async function apiFetch() {
     if (response.ok) {
       const data = await response.json();
       console.log(data); // testing only
-      // displayResults(data); // uncomment when ready
+      displayResults(data); // uncomment when ready
     } else {
         throw Error(await response.text());
     }
   } catch (error) {
       console.log(error);
   }
+}
+
+function displayResults(data) {
+  captionDesc.innerHTML = data.weather[0].description;
+  currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+  const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+  weatherIcon.setAttribute('src', iconsrc);
+  weatherIcon.setAttribute('alt', data.weather[0].description)
 }
 
 apiFetch();
